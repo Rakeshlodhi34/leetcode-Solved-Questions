ed36f6b93fa02214cc7a379f50be20cd88491332
Rakesh Lodhi <113932031+Rakeshlodhi34@users.noreply.github.com>
2023-08-21 23:02:12 +0530

    Added solution - LeetHub

---
+++ Surround the 1's - GFG/surround-the-1s.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int[][] matrix = new int[n][m];
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++)
+                    matrix[i][j] = Integer.parseInt(S[j]);
+            }
+            Solution ob = new Solution();
+            int ans = ob.Count(matrix);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int  Count(int[][] matrix)
+    {
+        // code here
+        int count = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 1){
+                    int currentZeroCount = 0;
+                    if(i-1>=0 && matrix[i-1][j]==0){
+                        currentZeroCount++;
+                    }
+                    if(j-1>=0 && matrix[i][j-1]==0){
+                        currentZeroCount++;
+                    }
+                    if(j-1>=0 && i+1<m && matrix[i+1][j-1]==0){
+                        currentZeroCount++;
+                    }
+                    if(i-1>=0 && j+1<n && matrix[i-1][j+1]==0){
+                        currentZeroCount++;
+                    }
+                    if(i-1>=0 && j-1>=0 && matrix[i-1][j-1]==0){
+                        currentZeroCount++;
+                    }
+                    if(j+1<n && matrix[i][j+1]==0){
+                        currentZeroCount++;
+                    }
+                    if(i+1<m && matrix[i+1][j]==0){
+                        currentZeroCount++;
+                    }
+                    if(i+1<m && j+1<n && matrix[i+1][j+1]==0){
+                        currentZeroCount++;
+                    }
+                    if(currentZeroCount%2==0 && currentZeroCount!=0){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
